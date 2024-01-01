18cb8d4238519b12716fabb051e45141e2e22b02
cat <catcatnovel@cat.com>
2024-01-01 11:25:43 +0800

    optimized code

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 from setuptools import setup, find_packages
 
+
+def readme(file_name: str):
+    try:
+        with open(file_name, encoding='utf-8') as f:
+            return f.read()
+    except:
+        return ''
+
+
 setup(
     name='hbookerLib',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     description='通过开源项目HbookerAppNovelDownloader代码修改而来的API库',
-    long_description=open('README.md').read(),
+    long_description=readme('README.md'),
     long_description_content_type='text/markdown',  # if you have a README.md
-    author='hang333',
+    author='catnovel',
     author_email='cat@cat.com',
     url='https://github.com/catnovel/hbookerLib',
+    license=readme('LICENSE'),
     install_requires=[
         'setuptools~=57.4.0',
         'requests~=2.26.0',
-        'crypto~=1.4.1',
+        'cryptography~=41.0.7'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
